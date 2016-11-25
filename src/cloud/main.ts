2a79ab3236566eef97b7a65fecc8e3ddf74f04ff
eclair <eclair.chang@oracle.com>
2016-11-25 23:26:49 +0800

    test Parse Query

--- src/cloud/main.ts
+++ src/cloud/main.ts
@@ -1,7 +1,6 @@
 
 import {ProjectService} from "./ProjectService";
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
   var prom = new ProjectService().queryProduct();
   prom.then((r) => res.success(JSON.stringify(r)))
 });
